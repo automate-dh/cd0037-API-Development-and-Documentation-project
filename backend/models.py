d86473edf468ac1d674b63861adc986abccc5743
Jubril Abdulsalam <abdulsalamjubr3@gmail.com>
2022-06-14 18:47:03 +0100

    .

--- backend/models.py
+++ backend/models.py
@@ -3,8 +3,10 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
+"""CHANGE below code to get info from enviroment """
 database_name = 'trivia'
-database_path = 'postgres://{}/{}'.format('localhost:5432', database_name)
+database_password = 'easydoesit'
+database_path = 'postgres://{}:{}@{}/{}'.format('postgres', database_password, 'localhost:5432', database_name)
 
 db = SQLAlchemy()
 
--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -6,6 +6,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+"""CHANGE below code to get info from enviroment """
+database_name = 'trivia'
+database_password = 'easydoesit'
+database_path = 'postgres://{}:{}@{}/{}'.format('postgres', database_password, 'localhost:5432', database_name)
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -15,7 +19,7 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = database_path
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -33,6 +37,12 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+
+    def test_get_categories(self):
+        """test /categories endpoint"""
+        res = self.client().get('/categories')
+
+        self.assertEqual(res.status_code, 200)
 
 
 # Make the tests conveniently executable
